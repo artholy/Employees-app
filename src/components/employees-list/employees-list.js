72dccd13a464c97d0737b007e16a633123e4ff1e
valeriya <valeriyastoyan26@gmail.com>
2023-12-19 21:06:33 +0300

    make server like data

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,17 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item=> {
+        return (
+            <EmployeesListItem name={item.name} salary={item.salary}/>
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem name="John C." salary="800"/>
-            <EmployeesListItem name="Alex N." salary="3000"/>
-            <EmployeesListItem name="Carl W." salary="5000"/>
+            {elements}
         </ul>
     )
 }
