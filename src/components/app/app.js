7535cc25d5d39cd6b558ecc2fc617cef33ab1f56
valeriya <valeriyastoyan26@gmail.com>
2024-01-03 21:37:44 +0300

    add method newItem for addition of new emp

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,12 +18,29 @@ class App extends Component {
         {name: "Carl W.", salary: 5000, increase: false, id: 3}
       ]
     }
+    this.maxId = 4;
   }
 
   deleteItem = (id) => {
     this.setState(({data}) => {
       return {
         data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary, 
+      increase: false,
+      id: this.maxId ++
+    }
+
+    this.setState(({data}) => {
+      const newArr = [...data, newItem]
+      return {
+        data: newArr
       }
     })
   }
