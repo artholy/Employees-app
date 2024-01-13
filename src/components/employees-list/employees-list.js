8218dc980a2b6bfce53d490008c838605dcc9ce7
valeriya <valeriyastoyan26@gmail.com>
2024-01-13 14:49:48 +0300

    add: create single metod instead of 2

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data, onDelete, onToggleIncrease, onToggleRise}) => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
     const elements = data.map(item=> {
 
@@ -13,8 +13,7 @@ const EmployeesList = ({data, onDelete, onToggleIncrease, onToggleRise}) => {
             key={id} 
             {...itemProps}
             onDelete={() => onDelete(id)}
-            onToggleIncrease={() => onToggleIncrease(id)}
-            onToggleRise={() => onToggleRise(id)}/>
+            onToggleProp={() => onToggleProp(id)}/>
         )
     })
 
